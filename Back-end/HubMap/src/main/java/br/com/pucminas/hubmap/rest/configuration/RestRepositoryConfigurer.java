ff78fda5a10f9247de511ddf5a8e48bb1f455944
Junio Almeida <junioferreiradealmeida2001@gmail.com>
2022-07-20 11:45:52 -0300

    Set CORS configurations and Handle exceptions before send to client

--- Back-end/HubMap/src/main/java/br/com/pucminas/hubmap/rest/configuration/RestRepositoryConfigurer.java
+++ Back-end/HubMap/src/main/java/br/com/pucminas/hubmap/rest/configuration/RestRepositoryConfigurer.java
@@ -1,8 +1,12 @@
 package br.com.pucminas.hubmap.rest.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import br.com.pucminas.hubmap.domain.comment.Comment;
@@ -15,5 +19,20 @@ public class RestRepositoryConfigurer implements RepositoryRestConfigurer{
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 		config.exposeIdsFor(Comment.class, Post.class, NGram.class);
+		cors.addMapping("/**")
+			.allowedOrigins("*")
+			.allowedMethods("GET", "POST", "PUT", "DELETE");
+	}
+	
+	@Bean
+	Validator validator() {
+		return new LocalValidatorFactoryBean();
+	}
+	
+	@Override
+	public void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener vrel) {
+		Validator validator = validator();
+		vrel.addValidator("beforeCreate", validator);
+		vrel.addValidator("beforeSave", validator);
 	}
 }
