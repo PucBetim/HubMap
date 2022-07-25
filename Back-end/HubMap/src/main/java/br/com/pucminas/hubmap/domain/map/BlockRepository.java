7b9a2fd1d784a6d7959061d39def9490884dfc34
Junio Almeida <junioferreiradealmeida2001@gmail.com>
2022-07-25 11:12:01 -0300

    Correct ID type in Repositories <Entity, TypeId> - Generics
    Necessary for it works fine

--- Back-end/HubMap/src/main/java/br/com/pucminas/hubmap/domain/map/BlockRepository.java
+++ Back-end/HubMap/src/main/java/br/com/pucminas/hubmap/domain/map/BlockRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BlockRepository extends CrudRepository<Block, Integer>{
+public interface BlockRepository extends CrudRepository<Block, BlockPK>{
 //TODO Check if this class will really inherit from CrudRepository	
 }
--- Back-end/HubMap/src/main/java/br/com/pucminas/hubmap/domain/post/NGramRepository.java
+++ Back-end/HubMap/src/main/java/br/com/pucminas/hubmap/domain/post/NGramRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NGramRepository extends JpaRepository<NGram, Integer>{
+public interface NGramRepository extends JpaRepository<NGram, NGramPK>{
 }
