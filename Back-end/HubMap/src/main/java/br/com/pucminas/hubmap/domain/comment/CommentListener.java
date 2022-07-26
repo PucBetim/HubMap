826c0a27fb10199e22f728797520e6199ec85b2f
Junio Almeida <junioferreiradealmeida2001@gmail.com>
2022-07-26 12:01:32 -0300

    Add logic to initialize Post and Comment with author

--- Back-end/HubMap/src/main/java/br/com/pucminas/hubmap/domain/comment/CommentListener.java
+++ Back-end/HubMap/src/main/java/br/com/pucminas/hubmap/domain/comment/CommentListener.java
@@ -2,13 +2,39 @@ package br.com.pucminas.hubmap.domain.comment;
 
 import javax.persistence.PrePersist;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+
+import br.com.pucminas.hubmap.domain.user.AppUser;
+import br.com.pucminas.hubmap.domain.user.AppUserRepository;
 
 @Component
 public class CommentListener {
 
+	
+	private static AppUserRepository appUserRepository;
+	
 	@PrePersist
-	public void onPrePersistComment(Comment comment) {
-		comment.initializeComment();
+	public void onPrePersistPost(Comment comment) {
+		
+		if(comment.getAuthor() == null) {
+			comment.initializeComment();
+			
+			String email = SecurityContextHolder.getContext().getAuthentication().getName();
+			AppUser appUser = appUserRepository.findByEmail(email);
+			
+			if(appUser == null) {
+				throw new UsernameNotFoundException("O usuário com e-mail '" + email + "' não foi encontrado.");
+			}
+			
+			comment.setAuthor(appUser);
+		}
+	}
+	
+	@Autowired
+	public void init(AppUserRepository appUserRepository) {
+		CommentListener.appUserRepository = appUserRepository;
 	}
 }
--- Back-end/HubMap/src/main/java/br/com/pucminas/hubmap/domain/post/PostListener.java
+++ Back-end/HubMap/src/main/java/br/com/pucminas/hubmap/domain/post/PostListener.java
@@ -2,13 +2,38 @@ package br.com.pucminas.hubmap.domain.post;
 
 import javax.persistence.PrePersist;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+
+import br.com.pucminas.hubmap.domain.user.AppUser;
+import br.com.pucminas.hubmap.domain.user.AppUserRepository;
 
 @Component
 public class PostListener {
-
+	
+	private static AppUserRepository appUserRepository;
+	
 	@PrePersist
 	public void onPrePersistPost(Post post) {
-		post.initializePost();
+		
+		if(post.getAuthor() == null) {
+			post.initializePost();
+			
+			String email = SecurityContextHolder.getContext().getAuthentication().getName();
+			AppUser appUser = appUserRepository.findByEmail(email);
+			
+			if(appUser == null) {
+				throw new UsernameNotFoundException("O usuário com e-mail '" + email + "' não foi encontrado.");
+			}
+			
+			post.setAuthor(appUser);
+		}
+	}
+	
+	@Autowired
+	public void init(AppUserRepository appUserRepository) {
+		PostListener.appUserRepository = appUserRepository;
 	}
 }
