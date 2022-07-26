07f8103d2836fbc97304582a1c5ae3b32c95a16b
Junio Almeida <junioferreiradealmeida2001@gmail.com>
2022-07-26 11:36:51 -0300

    Add isBlank method in StringUtils

--- Back-end/HubMap/src/main/java/br/com/pucminas/hubmap/utils/StringUtils.java
+++ Back-end/HubMap/src/main/java/br/com/pucminas/hubmap/utils/StringUtils.java
@@ -9,4 +9,12 @@ public class StringUtils {
 		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 		return encoder.encode(raw);
 	}
+	
+	public static boolean isBlank(String str) {
+		if(str == null || str.trim().length() == 0) {
+			return true;
+		}
+		
+		return false;
+	}
 }
