c890455a70b76f3e2aaa0dfec43ee62aa4c1fd64
Junio Almeida <junioferreiradealmeida2001@gmail.com>
2022-10-14 22:47:32 -0300

    Add custom tomcat server for production environment

--- server/HubMap/src/main/java/br/com/pucminas/hubmap/HubMapApplication.java
+++ server/HubMap/src/main/java/br/com/pucminas/hubmap/HubMapApplication.java
@@ -4,10 +4,17 @@ import static br.com.pucminas.hubmap.utils.LoggerUtils.getLoggerFromClass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HubMapApplication {
+public class HubMapApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HubMapApplication.class);
+	}
+	 
 	public static void main(String[] args) {
 		SpringApplication.run(HubMapApplication.class, args);
 		getLoggerFromClass(HubMapApplication.class).info("HubMap is ready!");
