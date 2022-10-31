8cf085637c2d9a31632baafc3f5e1db451fb42b0
Junio Almeida <junioferreiradealmeida2001@gmail.com>
2022-10-30 21:02:08 -0300

    Fix build.gradle dependencies and custom tomcat server logs

--- server/HubMap/src/main/java/br/com/pucminas/hubmap/HubMapApplication.java
+++ server/HubMap/src/main/java/br/com/pucminas/hubmap/HubMapApplication.java
@@ -15,7 +15,7 @@ public class HubMapApplication extends SpringBootServletInitializer {
 		getLoggerFromClass(HubMapApplication.class).info("HubMap is ready!");
 		return application.sources(HubMapApplication.class);
 	}
-	 
+
 	public static void main(String[] args) {
 		SpringApplication.run(HubMapApplication.class, args);
 		getLoggerFromClass(HubMapApplication.class).info("HubMap is ready!");
