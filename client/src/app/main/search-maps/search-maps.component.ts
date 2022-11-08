cd4aadec3a82ae0633b6a2a24ee85099a700c7cf
Carlos Silva <carloseduardosesilva@gmail.com>
2022-11-08 15:20:25 -0300

    commit

--- client/src/app/main/search-maps/search-maps.component.ts
+++ client/src/app/main/search-maps/search-maps.component.ts
@@ -23,7 +23,7 @@ export class SearchMapsComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = this.fb.group({
-      search: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(80)]],
+      search: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(80)]],
     });
   }
 
