fbcb92b080d957d31beb7216901d79f1d7df7598
Junio Almeida <junioferreiradealmeida2001@gmail.com>
2022-11-23 22:59:47 -0300

    Change default ordering for posts return to modified field descending

--- server/HubMap/src/main/java/br/com/pucminas/hubmap/infrastructure/web/controller/PublicController.java
+++ server/HubMap/src/main/java/br/com/pucminas/hubmap/infrastructure/web/controller/PublicController.java
@@ -26,41 +26,42 @@ public class PublicController {
 
 	@Autowired
 	private PostRepository postRepository;
-	
+
 	@Autowired
 	private CommentRepository commetRepository;
-	
+
 	@GetMapping(path = "/posts")
 	public ResponseEntity<List<Post>> getAllPublicPosts(
 			@RequestParam(required = false) Integer page,
-			@RequestParam(required = false) Integer size,
+			@RequestParam(required = false) Integer size, 
 			@RequestParam(required = false) Boolean descending,
 			@RequestParam(required = false) String... sort) {
-		
+
 		List<Post> posts = new ArrayList<>();
-		
-		if(size == null) {
+
+		if (size == null) {
 			size = 10;
 		}
-		
-		if(sort == null) {
-			sort = new String[]{"title", "id"};
+
+		if (sort == null) {
+			sort = new String[] { "modified" };
+			descending = true;
 		}
 		Pageable pageable = PageableUtils.getPageableFromParameters(page, size, descending, sort);
-		
+
 		postRepository.findAllPublic(pageable).forEach(posts::add);
 		posts.forEach(p -> {
 			p.setMapToShow();
 			p.getAuthor().setAuthorForPublicAccess();
 		});
-		
+
 		if (posts.isEmpty()) {
 			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 		}
 
 		return new ResponseEntity<>(posts, HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/posts/{id}")
 	public ResponseEntity<Post> getPostsById(@PathVariable Integer id) {
 		try {
@@ -73,42 +74,42 @@ public class PublicController {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 	}
-	
+
 	@GetMapping(path = "/comments")
 	public ResponseEntity<List<Comment>> getAllPublicComments(
-			@RequestParam(required = true, name = "post") Integer postId,
+			@RequestParam(required = true, name = "post") Integer postId, 
 			@RequestParam(required = false) Integer page,
-			@RequestParam(required = false) Integer size,
+			@RequestParam(required = false) Integer size, 
 			@RequestParam(required = false) Boolean descending,
 			@RequestParam(required = false) String... sort) {
-		
+
 		List<Comment> comments = new ArrayList<>();
-		
-		if(size == null) {
+
+		if (size == null) {
 			size = 10;
 		}
-		
-		if(sort == null) {
-			sort = new String[]{"timestamp"};
+
+		if (sort == null) {
+			sort = new String[] { "timestamp" };
 		}
 		Pageable pageable = PageableUtils.getPageableFromParameters(page, size, descending, sort);
-		
+
 		commetRepository.findByPostIfPublic(postId, pageable).forEach(comments::add);
 		comments.forEach(c -> c.getAuthor().setAuthorForPublicAccess());
-		
+
 		if (comments.isEmpty()) {
 			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 		}
 
 		return new ResponseEntity<>(comments, HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/comments/{id}")
 	public ResponseEntity<Comment> getCommentsById(@PathVariable Integer id) {
 		try {
 			Comment comment = commetRepository.findByIdIfPostPublic(id).orElseThrow();
 			comment.getAuthor().setAuthorForPublicAccess();
-			
+
 			return new ResponseEntity<>(comment, HttpStatus.OK);
 		} catch (NoSuchElementException e) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
